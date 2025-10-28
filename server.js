14505ca2e4a51813e054b734d96665e004238894
hermann <hermanike131@gmail.com>
2025-10-28 10:45:29 +0300

    Prepare for deployment to Render

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ app.post('/api/signup', (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
